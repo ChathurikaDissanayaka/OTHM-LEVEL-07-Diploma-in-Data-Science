d4db81978800f15d7302dd25bd39520592128010
Sachini Dissanayaka <61037110+ChathurikaDissanayaka@users.noreply.github.com>
2024-05-19 20:50:35 +0530

    Update example1.py

--- Advanced Predictive Modelling/Linear Regression/example1.py
+++ Advanced Predictive Modelling/Linear Regression/example1.py
@@ -11,6 +11,10 @@ y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 # execute a method that returns some important key values of liinear regression
 slope, intercept, r, p, std_err = stats.linregress(x,y)
 
+# print r value (range between -1 - 1)
+# closer to 0 - not sutable for linear regression
+print(r)
+
 # function that uses the slope and intercept values to return a new value 
 # this new value represents where on the y-axis the corresponding x value will be placed
 def myfunc(x):
